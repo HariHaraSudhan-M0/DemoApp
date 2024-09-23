4b5f5752d4c4205d538363c21d886268e2292b2e
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-09-23 07:45:55 +0530

    Initial Commit

---
+++ demoTests/demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  demoTests.swift
+//  demoTests
+//
+//  Created by HARIHARASUDHAN M on 23/09/24.
+//
+
+import Testing
+@testable import demo
+
+struct demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
